e97ee98ed352c88c0bad6e4ac8806c249bc8a8f2
sharreysuhendra22 <49983@jisedu.or.id>
2020-04-22 16:26:40 +0700

    Added spaceship reversed controls

---
+++ Game Jam 2/Assets/Scripts/SSController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SSController : MonoBehaviour
+{
+    void Update(){
+       transform.Translate(0f, Input.GetAxis("Horizontal")*10f*Time.deltaTime, 0f); 
+       transform.Translate(Input.GetAxis("Vertical")*10f*Time.deltaTime, 0f, 0f); 
+    }
+}
